dd7c1a37f1db3376576880c54a66aec72bff28e8
RahulM <rmtech.cloud@gmail.com>
2023-11-08 22:04:27 +1100

    pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
+import profile from './pages/profile'
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
+
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Signin" element={<Signin />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/profile" element={<profile />} />
+ 
+      </Routes>
+    </BrowserRouter>
   )
 }
